8f2f9f10f390aa6d3061caed1c739c6bf316c276
Your Name <rahmatrohmatdoni@gmail.com>
2026-05-25 10:28:21 +0800

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Mamat Ganteng
+            Mamat Keren
           </a>
         </div>
       </main>
